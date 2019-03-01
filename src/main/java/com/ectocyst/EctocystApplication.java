404a58ff381a978e6063d56938ca9f3fe6b88372
Seaguller <1656299466@qq.com>
2019-03-01 15:34:39 +0800

    项目初始化

--- src/main/java/com/ectocyst/EctocystApplication.java
+++ src/main/java/com/ectocyst/EctocystApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @ServletComponentScan
 @EnableCaching
 public class EctocystApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(EctocystApplication.class, args);
 	}
